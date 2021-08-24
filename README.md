159cae75f92b90588806da15c98c6c29ec290755
Jhon Jairo Hernandez <amonzos@protonmail.com>
2021-08-24 11:36:45 -0500

    Version final, falta css de registro

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Practica
+
+## Requerimientos
+
+- Para usar json server desde ejecutar el siguiente comando desde la carpeta del proyecto
+
+```bash
+json-server --watch data/productos.json --port 4000
+```
---
+++ scripts/regristro.js
@@ -0,0 +1,80 @@
+class UI {
+  constructor (){
+    this.tipos = ['Camisas', 'Jeans', 'Zapatos'];
+    this.urls = ['http://localhost:4000/camisas', 'http://localhost:4000/pantalones', 'http://localhost:4000/zapatos'];
+  }
+
+  validar(){
+    return true;
+  }
+
+  async guardar(){
+    const indice = document.getElementById("categorias").selectedIndex;
+    const nombre_ = document.getElementById('nombre').value;
+    const precio_ = parseFloat(document.getElementById('precio').value);
+
+    const data = await fetch(this.urls[indice], {
+      method: 'POST',
+      body: JSON.stringify({
+        img: 'Default',
+        nombre: nombre_,
+        precio: precio_,
+      }),
+      headers:{
+        "Content-Type": "application/json; charset=UTF-8"
+      }
+    })
+  }
+
+  async modificar(){
+    const indice = document.getElementById("categorias").selectedIndex;
+    const nombre_ = document.getElementById('nombre').value;
+    const precio_ = parseFloat(document.getElementById('precio').value);
+    const id_ = parseInt(document.getElementById('id').value);
+    const data = await fetch(`${this.urls[indice]}/${id_}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        nombre: nombre_,
+        precio: precio_,
+        id: id_,
+      }),
+      headers:{
+        "Content-Type": "application/json; charset=UTF-8"
+      }
+    })
+  }
+
+
+  async eliminar(){
+    const indice = document.getElementById("categorias").selectedIndex;
+    const id = document.getElementById('id').value;
+    const data = await fetch(`${this.urls[indice]}/${id}`, {
+      method: 'DELETE',
+    })
+  }
+}
+
+
+/* --------------------  Eventos ------------------*/
+
+document.getElementById('guardar').addEventListener('click', () =>{
+  const userInterface = new UI();
+  if(userInterface.validar()){
+    userInterface.guardar();
+  }
+});
+
+
+document.getElementById('modificar').addEventListener('click', () =>{
+  const userInterface = new UI();
+  if(userInterface.validar()){
+    userInterface.modificar();
+  }
+});
+
+document.getElementById('eliminar').addEventListener('click', () =>{
+  const userInterface = new UI();
+  if(userInterface.validar()){
+    userInterface.eliminar();
+  }
+});
